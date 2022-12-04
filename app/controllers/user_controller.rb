6cd330a54f6c6c9600c3c9a6b57ded169ccb4242
IDante <maxim.polyanskiy@mail.ru>
2022-12-04 18:02:11 +0300

    Add jwt auth

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,14 +1,54 @@
 class UserController < ApplicationController
+  before_action :authorize_request, except: :create
+  before_action :find_user, except: %i[create index]
+
+
+  def index
+    @users = User.all
+    render json: @users, status: :ok
+  end
 
 
   def show
-    ans = User.all
-    render :json => ans
+    render json: @user, status: :ok
   end
+
 
   def create
-    user = User.create(name: "David", occupation: "Code Artist")
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
   end
 
+
+  def update
+    unless @user.update(user_params)
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by_username!(params[:_username])
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'User not found' }, status: :not_found
+  end
+
+  def user_params
+    params.permit(
+      :email, :password, :password_confirmation
+    )
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
+  root 'session#home'
+
   get 'home/index', to: 'home#index'
 
-  resources :user
+  #User routes
+  resources :user, only: [:new, :create, :index, :show]
+
+  post '/auth/login', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
 
 
 
