c6b13d7f27f0ec122dfd7f8ff3161db0c6f1e175
IDante <maxim.polyanskiy@mail.ru>
2022-12-16 15:42:17 +0300

    fix migrations for local

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_16_120040) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_16_123808) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
