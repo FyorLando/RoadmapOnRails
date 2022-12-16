f868abbf1546db43628eaacf13eb43534232b51b
mlysenko27 <foxmarisharnd@gmail.com>
2022-12-16 17:01:51 +0300

    Add topic_rates relations

--- app/models/rates_module/topic_rate.rb
+++ app/models/rates_module/topic_rate.rb
@@ -1,5 +1,6 @@
 module RatesModule
   class TopicRate < ActiveRecord::Base
-
+    belongs_to :user, :class_name => 'UserModule::User', :foreign_key => 'created_user_id'
+    belongs_to :topic, :class_name => 'RoadmapsModule::Topic', :foreign_key => 'topic_id'
   end
 end
